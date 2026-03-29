8188ad50ddec48257a3c8982c5a0dcd6c4a1dc44
Matthew Lau <lau.cy.matthew@gmail.com>
2026-03-29 10:55:22 +0100

    fix: name

--- terraform/app/main.tf
+++ terraform/app/main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "this" {
-  name = "app-runner-vpc-network-1"
   cidr_block = "10.0.0.0/16"
   tags = merge(
     local.common_tags,
+    Name = "app-runner-vpc-network-1"
   )
 }
