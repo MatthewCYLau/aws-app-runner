1d73eb9af1a84d23cc2c329b23fac82e37f246c4
Matthew Lau <lau.cy.matthew@gmail.com>
2026-04-01 08:13:12 +0100

    feat: app runner

---
+++ terraform/app/app_runner.tf
@@ -0,0 +1,23 @@
+resource "aws_apprunner_service" "this" {
+  service_name = "aws-app-runner-service"
+
+  source_configuration {
+    authentication_configuration {
+      access_role_arn = aws_iam_role.apprunner_ecr_access.arn
+    }
+
+    image_repository {
+      image_configuration {
+        port = "8080"
+      }
+      image_identifier      = "${aws_ecr_repository.this.repository_url}:latest"
+      image_repository_type = "ECR"
+    }
+    auto_deployments_enabled = false
+  }
+
+  tags = merge(
+    local.common_tags,
+    { Name = "aws-app-runne-service" }
+  )
+}
--- terraform/app/iam.tf
+++ terraform/app/iam.tf
@@ -12,3 +12,23 @@ resource "aws_iam_policy" "rds_policy" {
   name   = "RDS_IAM_Auth_Policy"
   policy = data.aws_iam_policy_document.rds_iam_auth.json
 }
+
+resource "aws_iam_role" "apprunner_ecr_access" {
+  name = "apprunner-ecr-access-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "build.apprunner.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "apprunner_ecr_access_policy" {
+  role       = aws_iam_role.apprunner_ecr_access.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+}
