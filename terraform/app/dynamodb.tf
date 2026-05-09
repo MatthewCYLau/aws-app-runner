5be5ad7d1492bcc7af7032e332d3d977da60a190
Matthew Lau <lau.cy.matthew@gmail.com>
2026-05-09 14:29:18 +0100

    feat: pnl table

--- terraform/app/dynamodb.tf
+++ terraform/app/dynamodb.tf
@@ -19,3 +19,25 @@ resource "aws_dynamodb_table" "stock_positions" {
     { Name = "AWS App Dynamo DB" }
   )
 }
+
+resource "aws_dynamodb_table" "positions_pnl" {
+  name         = "positions_pnl"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "PositionId" # partition key
+  range_key    = "CreatedAt"  # sort key
+
+  attribute {
+    name = "PositionId"
+    type = "S"
+  }
+
+  attribute {
+    name = "CreatedAt"
+    type = "S"
+  }
+
+  tags = merge(
+    local.common_tags,
+    { Name = "AWS App Dynamo DB" }
+  )
+}
