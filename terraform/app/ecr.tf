6e976be48594c552e33401ee46a51fa2b77beddf
Matthew Lau <lau.cy.matthew@gmail.com>
2026-05-18 19:21:58 +0100

    feat: client ecr

--- terraform/app/ecr.tf
+++ terraform/app/ecr.tf
@@ -10,3 +10,16 @@ resource "aws_ecr_repository" "this" {
     { Name = "AWS ECS App private ECR" }
   )
 }
+
+resource "aws_ecr_repository" "client" {
+  name                 = "streamlit-dashboard"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+  tags = merge(
+    local.common_tags,
+    { Name = "Streamlit Dashboard private ECR" }
+  )
+}
