82b0f912394415669d07d7bceb67e37ec6fa8a7b
Matthew Lau <lau.cy.matthew@gmail.com>
2026-05-20 07:13:13 +0100

    spike: separate alb

--- terraform/app/loadbalancer.tf
+++ terraform/app/loadbalancer.tf
@@ -39,6 +39,29 @@ resource "aws_lb_target_group" "this" {
   }
 }
 
+resource "aws_lb" "client" {
+  name               = "streamlit-dashboard-alb"
+  subnets            = aws_subnet.public.*.id
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb.id]
+
+  tags = merge(
+    local.common_tags,
+    { Name = "streamlit-dashboard-alb" }
+  )
+}
+
+resource "aws_lb_listener" "client_http_forward" {
+  load_balancer_arn = aws_lb.client.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.client.arn
+  }
+}
+
 resource "aws_lb_target_group" "client" {
   name        = "streamlit-dashboard-alb-tg"
   port        = 80
@@ -52,11 +75,12 @@ resource "aws_lb_target_group" "client" {
     protocol            = "HTTP"
     matcher             = "200-299"
     timeout             = "20"
-    path                = "/client"
+    path                = "/"
     unhealthy_threshold = "2"
   }
 }
 
+/*
 resource "aws_lb_listener_rule" "client_routing" {
   listener_arn = aws_lb_listener.http_forward.arn
   priority     = 10
@@ -72,7 +96,12 @@ resource "aws_lb_listener_rule" "client_routing" {
     }
   }
 }
+*/
 
-output "name" {
+output "api_lb_dns_name" {
   value = aws_lb.this.dns_name
+}
+
+output "client_lb_dns_name" {
+  value = aws_lb.client.dns_name
 }
