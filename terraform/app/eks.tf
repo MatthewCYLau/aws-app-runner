9c9bb2e889d50ee7d6e8bc16de964363beac312c
Matthew Lau <lau.cy.matthew@gmail.com>
2026-07-15 16:28:22 +0100

    feat: eks

--- terraform/app/eks.tf
+++ terraform/app/eks.tf
@@ -39,6 +39,26 @@ module "eks" {
       instance_types = ["t3.medium"]
       capacity_type  = "ON_DEMAND"
     }
+
+    dev_nodes = {
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 1
+      instance_types = ["t3.medium"]
+
+      labels = {
+        np = "dev"
+      }
+
+      taints = [
+        {
+          key    = "np"
+          value  = "dev"
+          effect = "NO_SCHEDULE"
+        }
+      ]
+    }
+
   }
   # enable_irsa = true
   cluster_addons = {
@@ -46,6 +66,104 @@ module "eks" {
     amazon-cloudwatch-observability = {
       most_recent = true
     }
+    vpc-cni = {
+      most_recent = true
+      configuration_values = jsonencode({
+        enableNetworkPolicy = "true"
+      })
+    }
   }
+  node_security_group_additional_rules = {
+    ingress_self_all = {
+      description = "Allow nodes to communicate with each other"
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      type        = "ingress"
+      self        = true
+    }
+  }
+
+}
+
+
+locals {
+  oidc_url     = module.eks.cluster_oidc_issuer_url
+  oidc_repo    = replace(local.oidc_url, "https://", "")
+  provider_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${local.oidc_repo}"
+}
+
+data "aws_iam_policy_document" "irsa_trust" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = [local.provider_arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${local.oidc_repo}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${local.oidc_repo}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "lb_controller" {
+  name               = "aws-load-balancer-controller-irsa"
+  assume_role_policy = data.aws_iam_policy_document.irsa_trust.json
+}
+
+resource "aws_iam_role_policy_attachment" "lb_controller_attach" {
+  role       = aws_iam_role.lb_controller.name
+  policy_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/AWSLoadBalancerControllerIAMPolicy"
+}
+
+data "aws_iam_policy_document" "ebs_csi_trust" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = [local.provider_arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${local.oidc_repo}:sub"
+      values   = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${local.oidc_repo}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ebs_csi" {
+  name               = "AmazonEKS_EBS_CSI_DriverRole"
+  assume_role_policy = data.aws_iam_policy_document.ebs_csi_trust.json
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_attach" {
+  role       = aws_iam_role.ebs_csi.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEBSCSIDriverPolicyV2"
+}
+
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name             = module.eks.cluster_name
+  addon_name               = "aws-ebs-csi-driver"
+  service_account_role_arn = aws_iam_role.ebs_csi.arn
 }
 */
