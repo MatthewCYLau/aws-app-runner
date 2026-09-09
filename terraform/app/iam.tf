3f08b26dc2f23e82dcf5bb9655cb656492b403a9
Matthew Lau <lau.cy.matthew@gmail.com>
2026-09-09 10:31:05 +0100

    chore: comment

--- terraform/app/iam.tf
+++ terraform/app/iam.tf
@@ -307,6 +307,7 @@ resource "aws_iam_role_policy" "lambda_policy" {
   })
 }
 
+/*
 resource "aws_eks_pod_identity_association" "s3_access" {
   cluster_name    = module.eks.cluster_name
   namespace       = "dev"
@@ -345,3 +346,4 @@ resource "aws_eks_pod_identity_association" "cloudwatch_observability" {
   service_account = "cloudwatch-agent"
   role_arn        = aws_iam_role.cloudwatch_observability.arn
 }
+*/
